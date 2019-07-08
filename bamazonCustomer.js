a7f1ff1820e33a78bfab955f42fae16e112d58ee
Edward Mara <e.james.mara@gmail.com>
2019-07-07 21:44:50 -0700

    implemented cli-table to diplay products in a formatted table on the CLI

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,9 @@
 require("dotenv").config();
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require("cli-table");
+
+
 
 var connection = mysql.createConnection({
     host: "localhost",
@@ -16,17 +19,24 @@ var connection = mysql.createConnection({
 connection.connect(function (err) {
     if (err) throw err;
     queryProducts();
-    
+
 });
 
 function queryProducts() {
-    connection.query("SELECT * FROM products", function (err, results) {
+    connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        console.log(results);
+        var table = new Table({
+            head: ["item_id", "product_name", "department_name", "price", "stock-quantity"],
+            colWidths: [10,25,25,10,15]
+        });
+        for (var i=0 ; i<res.length; i++){
+            table.push([res[i].item_id, res[i].product_name, res[i].department_name, res[i].price, res[i].stock_quantity]);
+        }
+        console.log(table.toString());
         buyProduct();
-        
+
     })
-    
+
 }
 
 function buyProduct() {
@@ -42,63 +52,61 @@ function buyProduct() {
             message: "How many would you like to buy?"
         }
 
-    ]).then(function(answer) {
+    ]).then(function (answer) {
         var buyId = answer.buyId;
         var buyQuantity = answer.buyQuantity;
 
-        connection.query("SELECT * FROM products WHERE ?",{item_id: buyId},function(err, res){
+        connection.query("SELECT * FROM products WHERE ?", { item_id: buyId }, function (err, res) {
             if (err) throw err;
             // console.log(res[0].stock_quantity);
             var quantity = res[0].stock_quantity;
-            if(buyQuantity <= quantity){
+            if (buyQuantity <= quantity) {
                 var cost = buyQuantity * res[0].price;
                 var updateQuantity = quantity - buyQuantity;
                 console.log(`That will cost $${cost}, and there are ${updateQuantity} left.`);
                 connection.query("UPDATE products SET ? WHERE ?",
-                [
-                    {
-                        stock_quantity: updateQuantity
-                    },
-                    {
-                        item_id: buyId
-                    }
-                ]);
-                //TODO: ask user if to go back to item list or end connection
+                    [
+                        {
+                            stock_quantity: updateQuantity
+                        },
+                        {
+                            item_id: buyId
+                        }
+                    ]);
                 inquirer.prompt(
                     {
-                        name:"reset",
-                        type:"list",
+                        name: "reset",
+                        type: "list",
                         choices: ["Yes", "No"],
                         message: "Would you like to continue shopping?"
 
                     }
-                ).then(function(answer){
-                    if (answer.reset === "Yes"){
+                ).then(function (answer) {
+                    if (answer.reset === "Yes") {
                         queryProducts();
-                    }else{
+                    } else {
                         connection.end();
                     }
                 });
-                
-            }else{
+
+            } else {
                 console.log("Insufficient Stock!")
-                //TODO: ask user if to go back to item list or end connection
                 inquirer.prompt(
                     {
-                        name:"reset",
-                        type:"list",
+                        name: "reset",
+                        type: "list",
                         choices: ["Yes", "No"],
                         message: "Would you like to continue shopping?"
 
                     }
-                ).then(function(answer){
-                    if (answer.reset === "Yes"){
+                ).then(function (answer) {
+                    if (answer.reset === "Yes") {
                         queryProducts();
-                    }else{
+                    } else {
                         connection.end();
                     }
                 });
-                
+
             }
         });
     })
